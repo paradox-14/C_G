527bb4d22a89a4570df7a4f988d862f6c22e7b27
PaRaDoX-14 <ronik-cobaka@mail.ru>
2018-09-17 10:41:46 +0300

    Test

---
+++ WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,165 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(278, 301);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(312, 12);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(278, 301);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(80, 387);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(118, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Преобразовать";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(349, 349);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(207, 23);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Разность полутоновых изображений";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(688, 349);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(97, 23);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "Гистограмма";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Checked = true;
+            this.radioButton1.Location = new System.Drawing.Point(12, 332);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(91, 17);
+            this.radioButton1.TabIndex = 8;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Равные веса";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(12, 355);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(185, 17);
+            this.radioButton2.TabIndex = 9;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Разный вклад в интенсивность";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Location = new System.Drawing.Point(607, 12);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(278, 301);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 10;
+            this.pictureBox3.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(897, 422);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+    }
+}
+
---
+++ WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        Bitmap b_image;
+        Bitmap black_white1;
+        Bitmap black_white2;
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {      	
+            OpenFileDialog open_dialog = new OpenFileDialog();
+            open_dialog.Filter = "Image Files(*.BMP;*.JPG;*.GIF;*.PNG)|*.BMP;*.JPG;*.GIF;*.PNG|All files (*.*)|*.*"; 
+            if (open_dialog.ShowDialog() == DialogResult.OK) 
+            {
+              try
+              {
+                  b_image = new Bitmap(open_dialog.FileName);
+                  pictureBox1.Image = b_image;
+                  pictureBox1.Invalidate();
+             }
+             catch
+             {
+                  DialogResult rezult = MessageBox.Show("Невозможно открыть выбранный файл",
+                     "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+             }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (pictureBox1.Image != null) 
+            {
+                black_white1 = new Bitmap(b_image.Width, b_image.Height);
+                black_white2 = new Bitmap(b_image.Width, b_image.Height);
+
+                for (int j = 0; j < b_image.Height; j++)
+                    for (int i = 0; i < b_image.Width; i++)
+                    {
+                        UInt32 pixel = (UInt32)(b_image.GetPixel(i, j).ToArgb());
+
+                        float R = (float)((pixel & 0x00FF0000) >> 16); 
+                        float G = (float)((pixel & 0x0000FF00) >> 8); 
+                        float B = (float)(pixel & 0x000000FF); 
+                        
+                        if (radioButton1.Checked)
+                        {
+                            R = G = B = (R + G + B) / 3.0f;
+                            UInt32 newPixel = 0xFF000000 | ((UInt32)R << 16) | ((UInt32)G << 8) | ((UInt32)B);
+                            black_white1.SetPixel(i, j, Color.FromArgb((int)newPixel));
+                        }       
+                        else
+                        {
+                            R = G = B = (float)(0.2123 * (double)R + 0.7152 * (double)G + 0.0722 * (double)B) / 3.0f;
+                            UInt32 newPixel = 0xFF000000 | ((UInt32)R << 16) | ((UInt32)G << 8) | ((UInt32)B);
+                            black_white2.SetPixel(i, j, Color.FromArgb((int)newPixel));
+                        }
+                     
+                        
+                    }
+                if (radioButton1.Checked)
+                 pictureBox2.Image = black_white1;
+                else
+                    pictureBox3.Image = black_white2;
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Bitmap sub_image = new Bitmap(b_image.Width, b_image.Height);
+
+            for (int j = 0; j < b_image.Height; j++)
+                    for (int i = 0; i < b_image.Width; i++)
+                    {
+                        Color pixel1 = black_white1.GetPixel(i, j);
+                        Color pixel2 = black_white2.GetPixel(i, j); 
+                        UInt32 sub_pixel = (UInt32)Math.Max(pixel1.R - pixel2.R + pixel1.G - pixel2.G + pixel1.B - pixel2.B,
+                                                    pixel2.R - pixel1.R + pixel2.G - pixel1.G + pixel2.B - pixel1.B);
+                        sub_image.SetPixel(i, j, Color.FromArgb((int)sub_pixel));
+                    }
+            pictureBox1.Image = sub_image;
+        }
+    }
+}
